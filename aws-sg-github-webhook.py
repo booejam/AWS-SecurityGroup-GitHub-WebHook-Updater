9a3d70aaacbcb8ffbc9d4157e178c4cfb1854d0c
Izham Hakim <izhamidrs@gmail.com>
2025-07-17 17:40:01 +0800

    Initial commit

---
+++ aws-sg-github-webhook.py
@@ -0,0 +1,167 @@
+import os
+import boto3
+import requests
+
+
+def get_github_ip_list():
+    """
+    Call the GitHub API to fetch their server IPs used for webhooks
+
+    :rtype: list
+    :return: List of IPs
+    """
+    response = requests.get('https://api.github.com/meta')
+    ips = response.json()
+    if 'hooks' in ips:
+        return ips['hooks']
+
+    raise ConnectionError("Error loading IPs from GitHub")
+
+
+def get_aws_security_group(group_id):
+    """
+    Return the defined Security Group
+
+    :param group_id:
+    :type group_id: str
+    :return:
+    """
+    ec2 = boto3.resource('ec2')
+    group = ec2.SecurityGroup(group_id)
+    if group.group_id == group_id:
+        return group
+
+    raise ConnectionError('Failed to retrieve security group from Amazon')
+
+
+def clear_security_group_rules(group):
+    """
+    Remove all existing ingress and egress rules from the security group
+
+    :param group:
+    :return:
+    """
+    # Revoke all ingress rules
+    if group.ip_permissions:
+        group.revoke_ingress(IpPermissions=group.ip_permissions)
+        print("Cleared all existing ingress rules")
+
+    # Revoke all egress rules // I think this one can remove, engress should be 
+    if group.ip_permissions_egress:
+        group.revoke_egress(IpPermissions=group.ip_permissions_egress)
+        print("Cleared all existing egress rules")
+
+
+def add_ingress_rule(group, address, port, description):
+    """
+    Add the IP address and port to the security group
+
+    :param group:
+    :param address:
+    :param port:
+    :param description:
+    :return:
+    """
+    if "." in address:
+        permissions = [
+            {
+                'IpProtocol': 'tcp',
+                'FromPort': port,
+                'ToPort': port,
+                'IpRanges': [
+                    {
+                        'CidrIp': address,
+                        'Description': description,
+                    }
+                ],
+            }
+        ]
+    else:
+        permissions = [
+            {
+                'IpProtocol': 'tcp',
+                'FromPort': port,
+                'ToPort': port,
+                'Ipv6Ranges': [
+                    {
+                        'CidrIpv6': address,
+                        'Description': description,
+                    }
+                ],
+            }
+        ]
+    group.authorize_ingress(IpPermissions=permissions)
+    print(("Ingress rule from IP %s to Port %i has been added" % (address, port)))
+
+
+def add_egress_rule(group, address, port, description):
+    """
+    Add the IP address and port to the security group
+
+    :param group:
+    :param address:
+    :param port:
+    :param description:
+    :return:
+    """
+    if "." in address:
+        permissions = [
+            {
+                'IpProtocol': 'tcp',
+                'FromPort': port,
+                'ToPort': port,
+                'IpRanges': [
+                    {
+                        'CidrIp': address,
+                        'Description': description,
+                    }
+                ],
+            }
+        ]
+    else:
+        permissions = [
+            {
+                'IpProtocol': 'tcp',
+                'FromPort': port,
+                'ToPort': port,
+                'Ipv6Ranges': [
+                    {
+                        'CidrIpv6': address,
+                        'Description': description,
+                    }
+                ],
+            }
+        ]
+    group.authorize_egress(IpPermissions=permissions)
+    print(("Egress rule to IP %s from Port %i has been added" % (address, port)))
+
+
+def lambda_handler(event, context):
+    """
+    AWS lambda main func
+
+    :param event:
+    :param context:
+    :return:
+    """
+    ingress_ports = [int(port) for port in os.environ['INGRESS_PORTS_LIST'].split(",")]
+    if not ingress_ports:
+        ingress_ports = [443]
+
+    egress_ports = [int(port) for port in os.environ['EGRESS_PORTS_LIST'].split(",")]
+    if not egress_ports:
+        egress_ports = [22]
+
+    security_group = get_aws_security_group(os.environ['SECURITY_GROUP_ID'])
+    ip_addresses = get_github_ip_list()
+    description = "GitHub"
+
+    # Clear all existing rules
+    clear_security_group_rules(security_group)
+
+    # Add new rules
+    for ip_address in ip_addresses:
+        for port in ingress_ports:
+            add_ingress_rule(security_group, ip_address, port, description)
+        for port in egress_ports:
+            add_egress_rule(security_group, ip_address, port, description)
